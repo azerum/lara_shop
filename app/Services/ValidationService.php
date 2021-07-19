eb01b7fdaa6b2464e685f77d6b847e3cfa78b684
Artem Korotenko <whyareyoureadingmy@gmail.com>
2021-07-19 16:50:39 +0300

    chore: suppress false warnings in ValidationService

--- app/Services/ValidationService.php
+++ app/Services/ValidationService.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Services;
 
 use App\Exceptions\ValidationFailedException;
@@ -8,6 +7,8 @@ use Illuminate\Support\Facades\Validator;
 
 class ValidationService
 {
+    /** @noinspection PhpDocMissingThrowsInspection  */
+
     /**
      * @param string[] $rules
      * @throws ValidationFailedException
@@ -18,6 +19,11 @@ class ValidationService
         if ($validator->fails()) {
             throw new ValidationFailedException($validator->errors()->toArray());
         }
+
+        /** @noinspection PhpUnhandledExceptionInspection*/
+        //$validator->validated() throws only if $validator->fails() is true,
+        //but if it's true previous if block throws exception
+        //and returns from method
 
         return $validator->validated();
     }
