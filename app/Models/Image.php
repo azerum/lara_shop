6f12fda9751ab2aecf9610528568aa94949a737f
Artem Korotenko <whyareyoureadingmy@gmail.com>
2021-07-19 18:57:45 +0300

    add: src attribute for Image
    fix: $timestamps = false for Image
    
    update: $hidden for Image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,22 +6,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @property-read int $id
  * @property string $title
  * @property int $file_id
+ * @property File $file
  * @mixin \Illuminate\Database\Eloquent\Builder
  */
 class Image extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
+
     protected $fillable = [
         'title',
         'file_id',
         'album_id'
     ];
+
+    protected $hidden = [
+        'file_id',
+        'album_id',
+        'file'
+    ];
+
+    protected $appends = [
+        'src'
+    ];
+
+    public function getSrcAttribute(): string
+    {
+        return Storage::url($this->file->path);
+    }
 
     public function album(): BelongsTo
     {
