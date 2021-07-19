4a6eab6e5e77069bd95112d38372636b00a5c949
Artem Korotenko <whyareyoureadingmy@gmail.com>
2021-07-19 15:50:17 +0300

    refactor: fix typos, import classes in FactoriesHelpers

--- app/Helpers/FactoriesHelpers.php
+++ app/Helpers/FactoriesHelpers.php
@@ -2,24 +2,28 @@
 
 namespace App\Helpers;
 
+use InvalidArgumentException;
+use ReflectionClass;
+use ReflectionException;
+
 class FactoriesHelpers
 {
     /**
      * @param string $className
      * @param string $prefix
      * @return array
-     * @throws \InvalidArgumentException if class with name $className does not exists
+     * @throws InvalidArgumentException if class with name $className does not exist
      */
     public static function getClassConstantsByPrefix(string $className, string $prefix): array
     {
         $constants = null;
 
         try {
-            $reflection = new \ReflectionClass($className);
+            $reflection = new ReflectionClass($className);
             $constants = $reflection->getConstants();
         }
-        catch (\ReflectionException) {
-            throw new \InvalidArgumentException("Class with name '$className' does not exists.");
+        catch (ReflectionException) {
+            throw new InvalidArgumentException("Class with name '$className' does not exists.");
         }
 
         $filteredByPrefix = array_filter(
