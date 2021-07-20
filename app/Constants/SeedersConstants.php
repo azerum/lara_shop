150da755821d7926f8a481b2981e29e066cade56
Artem Korotenko <whyareyoureadingmy@gmail.com>
2021-07-20 18:24:43 +0300

    update: SeedersConstants for very small database

--- app/Constants/SeedersConstants.php
+++ app/Constants/SeedersConstants.php
@@ -4,14 +4,14 @@ namespace App\Constants;
 
 class SeedersConstants
 {
-    public const USERS_COUNT = 100;
+    public const USERS_COUNT = 50;
 
     public const CATEGORIES_COUNT = 5;
     public const SUBCATEGORIES_COUNT = 10;
 
-    public const PRODUCTS_COUNT = 100;
-    public const ORDERS_COUNT = 150;
-    public const MAX_PRODUCTS_PER_ORDER = 20;
+    public const PRODUCTS_COUNT = 50;
+    public const ORDERS_COUNT = 100;
+    public const MAX_PRODUCTS_PER_ORDER = 5;
 
     public const TRANSACTIONS_COUNT = self::ORDERS_COUNT;
 }
